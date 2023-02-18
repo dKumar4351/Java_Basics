810b30ff5b14e3ad1d4449e0ed4473ecf9dffc02
Deepak Kumar <deepakkumar@Deepaks-MacBook-Air.local>
2023-02-18 15:09:26 +0530

    okHttp and build a fat jar
    Signed-off-by: Deepak Kumar <deepakkumar@Deepaks-MacBook-Air.local>

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(Lib.getGreeting());
+        var httpLib = new HttpLib();
+
+        System.out.println(httpLib.getExampleDotCom());
     }
 }
